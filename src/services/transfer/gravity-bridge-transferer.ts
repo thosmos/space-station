d3520766c94d7ea3890338ba070f8ed91c5ed4f1
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2023-03-27 02:56:33 -0700

    Use grav api for WETH price

--- src/services/transfer/gravity-bridge-transferer.ts
+++ src/services/transfer/gravity-bridge-transferer.ts
@@ -182,6 +182,25 @@ async function getGasPrices (): Promise<{ slow: number; fast: number; instant: n
   }
 }
 
+async function getWETHPrice (): Promise<{ price: number }> {
+  try {
+    const response = await axios.get('https://info.gravitychain.io:9000/erc20_metadata');
+    const data = response.data;
+
+    const tokenData = data.find((item: any) => item.symbol === 'WETH');
+
+    if (!tokenData) {
+      throw new Error('WETH not found in the response');
+    }
+
+    return {
+      price: tokenData.exchange_rate / 1e6 // Convert to USD
+    };
+  } catch (error) {
+    throw new Error('Error fetching WETH price:');
+  }
+}
+
 async function getFees (fromChain: SupportedChain, token: IToken): Promise<BridgeFee[]> {
   if (fromChain === SupportedChain.GravityBridge) {
     try {
@@ -192,8 +211,8 @@ async function getFees (fromChain: SupportedChain, token: IToken): Promise<Bridg
       const tokenPriceData = await fetchTokenPriceData(token);
       const tokenPrice = tokenPriceData.price;
 
-      const ethPriceResponse = await axios.get('https://beaconcha.in/api/v1/execution/gasnow');
-      const ethPrice = ethPriceResponse.data.data.priceUSD;
+      const wethPriceData = await getWETHPrice();
+      const ethPrice = wethPriceData.price;
 
       if (token.erc20) {
         const erc20Token = token.erc20;
